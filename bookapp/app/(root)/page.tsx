16b9ed26405c45d0509342336e9c1fc7a8068676
MinKhantKyaw <minkhant050603@gmail.com>
2025-04-05 22:38:01 +0700

    Start Header, Bookoverview, booklist

---
+++ bookapp/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+
+const Home =()=> (
+    <>
+      <BookOverview/>
+      <BookList/>
+    </>
+)
+
+export default Home;
