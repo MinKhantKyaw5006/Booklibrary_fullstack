a6e36fae3e424416b424f6d6091f5153f71aae43
MinKhantKyaw <minkhant050603@gmail.com>
2025-04-06 21:35:33 +0700

    auth from ui

--- bookapp/app/(auth)/sign-in/page.tsx
+++ bookapp/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,18 @@
+'use client';
+import AuthForm from '@/components/AuthForm'
+import { signInSchema } from '@/lib/validations'
 import React from 'react'
 
-const page = () => {
-  return (
-    <div>
-      Sign-in
-    </div>
-  )
-}
+const page = () => (
+  <AuthForm
+  type= "SIGN_IN"
+  schema={signInSchema}
+  defaultValues ={{
+    email: "",
+    password: "",
+  }}
+  onSubmit={()=>{}}
+  />
+)
 
 export default page
