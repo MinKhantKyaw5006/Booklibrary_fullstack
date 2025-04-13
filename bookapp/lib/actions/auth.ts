e66966b08875818443cacd90411f77b2c7e7ad8b
MinKhantKyaw <minkhant050603@gmail.com>
2025-04-14 01:03:25 +0700

    fix error of workflow

--- bookapp/lib/actions/auth.ts
+++ bookapp/lib/actions/auth.ts
@@ -64,7 +64,7 @@ export const signUp = async (params: AuthCredentials)=>{
 
         //the resend function after signup
         await workflowClient.trigger({
-            url:`${config.env.prodApiEndpoint}/api/workflow/onboarding`,
+            url:`${config.env.prodApiEndpoint}/api/workflows/onboarding`,
             body:{
                 email,
                 fullName,
